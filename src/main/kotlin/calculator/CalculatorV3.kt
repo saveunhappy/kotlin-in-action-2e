c5e73819291e4d40905de1624018ceca68bdf4e1
houjiangtao <925147954@qq.com>
2024-09-28 19:56:46 +0800

    1.大数加法

--- src/main/kotlin/calculator/CalculatorV3.kt
+++ src/main/kotlin/calculator/CalculatorV3.kt
@@ -19,7 +19,7 @@ class CalculatorV3 {
         while (true) {
             println(help)
 
-            val input = readLine() ?: continue
+            val input = readlnOrNull() ?: continue
             val result = calculate(input)
 
             if (result == null) {
@@ -50,23 +50,36 @@ class CalculatorV3 {
 
     private fun addString(leftNum: String, rightNum: String): String {
         val result = StringBuilder()
+        //135 + 99
+        //字符串的长度减1正好是数组的下标最大值，所以获得的就是数字是有几位
+        //135.length是3，那leftIndex就是2
         var leftIndex = leftNum.length - 1
+        //99.length - 1就是1
         var rightIndex = rightNum.length - 1
+        //表示进位
         var carry = 0
 
         while (leftIndex >= 0 || rightIndex >= 0) {
+            //这里取的就是字符串的最后一位，那么就是个位数，5
             val leftVal = if (leftIndex >= 0) leftNum[leftIndex].digitToInt() else 0
+            //这里就是9，如果到百位，99没有百位，那么就是0
             val rightVal = if (rightIndex >= 0) rightNum[rightIndex].digitToInt() else 0
+            //两个数字个位相加，再加上进位
             val sum = leftVal + rightVal + carry
+            //除以10只需要知道进位即可
             carry = sum / 10
+            //取余数，进位余下的数字
             result.append(sum % 10)
+            //取十位
             leftIndex--
+            //取十位
             rightIndex--
         }
         if (carry != 0) {
+            //如果两个二位数相加超过100，这里就会有用
             result.append(carry)
         }
-
+        //字符串拼接的是从个位到百位，所以需要翻转
         return result.reverse().toString()
     }
 
@@ -101,7 +114,7 @@ class CalculatorV3 {
     }
 
     private fun parseOperator(input: String): Operation? {
-        Operation.values().forEach {
+        Operation.entries.forEach {
             if (input.contains(it.value)) {
                 return it
             }
